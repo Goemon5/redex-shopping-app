7c6a5b085a0f09780e1b160365ceafedbd90dc8c
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2023-02-27 20:04:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
+import CartContainer from './components/CartContainer';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
+import Modal from './components/Modal';
 
 function App() {
+  const {cartItems} =useSelector((state) => state.cart)
+  const dispatch =useDispatch();
+  const {isOpen} = useSelector((state) =>state.modal)
+  useEffect(() =>{
+   dispatch(calculateTotals())
+  },[cartItems])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {isOpen && <Modal />}
+      
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
